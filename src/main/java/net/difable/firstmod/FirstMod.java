8dc3755b3835a7009aefa33f9aa992e95fced498
Difable <martin.mason@hispeed.ch>
2022-08-31 16:39:23 +0200

    test commit and Push

--- src/main/java/net/difable/firstmod/FirstMod.java
+++ src/main/java/net/difable/firstmod/FirstMod.java
@@ -10,6 +10,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+//Very Important comment
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(FirstMod.MOD_ID)
 public class FirstMod
