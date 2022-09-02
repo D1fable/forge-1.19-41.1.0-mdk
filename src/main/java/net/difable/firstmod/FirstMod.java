e0b18079cd88c07e57344e83f8031ff01259d5b1
Difable <martin.mason@hispeed.ch>
2022-09-02 19:08:22 +0200

    Is now the same to the end of Tutorial 3.

--- src/main/java/net/difable/firstmod/FirstMod.java
+++ src/main/java/net/difable/firstmod/FirstMod.java
@@ -1,6 +1,7 @@
 package net.difable.firstmod;
 
 import com.mojang.logging.LogUtils;
+import net.difable.firstmod.block.ModBlocks;
 import net.difable.firstmod.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -24,6 +25,7 @@ public class FirstMod
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
