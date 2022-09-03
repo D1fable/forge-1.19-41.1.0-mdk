3ed09bfea2048379d1776b9f1c9f006fbf8e7f5b
Difable <martin.mason@hispeed.ch>
2022-09-03 20:18:31 +0200

    Updated to Tut 5, Working

--- src/main/java/net/difable/firstmod/item/ModItems.java
+++ src/main/java/net/difable/firstmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.difable.firstmod.item;
 
 import net.difable.firstmod.FirstMod;
+import net.difable.firstmod.item.custom.EightBallItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +16,8 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModTab.TUTORIAL_TAB)));
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
             () -> new Item(new Item.Properties().tab(ModCreativeModTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> EIGHT_BALL = ITEMS.register("eight_ball",
+            () -> new EightBallItem(new Item.Properties().tab(ModCreativeModTab.TUTORIAL_TAB).stacksTo(1)));
 
     public static void register(IEventBus eventBus)
     {
