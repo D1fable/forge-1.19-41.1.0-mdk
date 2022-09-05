b858a41b24724063aa8fc95c2dcda1d61d5db481
Difable <martin.mason@hispeed.ch>
2022-09-05 15:14:27 +0200

    Updated to tutorial 7

--- src/main/java/net/difable/firstmod/item/custom/EightBallItem.java
+++ src/main/java/net/difable/firstmod/item/custom/EightBallItem.java
@@ -1,6 +1,8 @@
 package net.difable.firstmod.item.custom;
 
 import net.difable.firstmod.sound.ModSounds;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
@@ -9,12 +11,27 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class EightBallItem extends Item {
     public EightBallItem(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        if(Screen.hasShiftDown()){
+            components.add(Component.literal("AHHHHHHHHHHH").withStyle(ChatFormatting.AQUA));
+        }else{
+            components.add(Component.literal("Press SHIFT for more info").withStyle(ChatFormatting.YELLOW));
+        }
+
+        super.appendHoverText(itemStack, level, components, flag);
     }
 
     @Override
